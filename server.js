f3ccaf834a022abaf75508cb206c87b606136381
Stacey <sseronic@uci.edu>
2017-05-09 14:57:43 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+/* Include static file web library */
+var static = require('node-static');
+
+/* Include http server library */
+var http = require('http');
+
+/* Assume we are running on Heroku */
+var port = process.env.PORT;
+ver directory = __dirname + '/public';
+
+/* If we aren't on Heroku, then we need to adjust the port and directory conformation and we klnow that because port won't be set */
+if (typeof port == 'undefined' || !port){
+  directory = './public';
+  port = 8080;
+  
+  /* Set up a static web server which wil deliver files from the filesystem */
+  var file = new.static.Server(directory);
+  
+  /* Construct an http server that gets files form the file server */
+  var app = http.createServer(
+    function(request,response){
+      request.addListener ('end',
+        function(){
+          file.server(request,response);
+          }
+       ).resume();
+    }
+  ).listen(port);        
